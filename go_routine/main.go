8311b8f93b635294494f6a6ecacea73a3b6b0c4b
chenshukang <chenshukang>
2024-09-21 15:50:07 +0800

    go routine

--- go_routine/main.go
+++ go_routine/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math/rand/v2"
 	"sync"
 	"time"
 )
 
+var m = sync.Mutex{}
 var wg = sync.WaitGroup{}
 var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
 var results = []string{}
@@ -24,9 +24,11 @@ func main() {
 }
 
 func dbCall(i int) {
-	var delay float32 = rand.Float32() * 2000
+	var delay float32 = 2000
 	time.Sleep(time.Duration(delay) * time.Millisecond)
 	fmt.Println("The result from the database is:", dbData[i])
+	m.Lock()
 	results = append(results, dbData[i])
+	m.Unlock()
 	wg.Done()
 }
