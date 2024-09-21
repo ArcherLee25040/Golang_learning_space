8f15ae0fb9e82bfdd4bae154bf676a1f767a63f5
chenshukang <chenshukang>
2024-09-21 18:02:31 +0800

    channels

--- channels/main.go
+++ channels/main.go
@@ -1,18 +1,52 @@
+// 定义功能：定义网站监听商品属性变化情况
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+	"time"
+)
+
+var MAX_Chicken_price float32 = 5
+var MAX_TOFU_price float32 = 3
 
 func main() {
-	var c = make(chan int)
-	go process(c)
-	for i := 0; i < 5; i++ {
-		fmt.Println(<-c)
+	var chickenchannel = make(chan string)
+	var tofuchannel = make(chan string)
+	var websites = []string{"walmart.com", "costco.com", "wholefoods.com"}
+	for i := range websites {
+		go Check_Chicken_Prices(websites[i], chickenchannel)
+		go Check_Tofu_Prices(websites[i], tofuchannel)
 	}
+	sendMessage(chickenchannel, tofuchannel)
 }
 
-func process(c chan int) {
-	for i := 0; i < 5; i++ {
-		c <- i
+func Check_Tofu_Prices(websites string, tofuchannel chan string) {
+	for {
+		time.Sleep(time.Second * 1)
+		var tofu_price = rand.Float32() * 20
+		if tofu_price <= MAX_TOFU_price {
+			tofuchannel <- websites
+			break
+		}
 	}
+
+}
+
+func Check_Chicken_Prices(websites string, chickenchannel chan string) {
+	for {
+		time.Sleep(time.Second * 1)
+		var chicken_price = rand.Float32() * 20
+		if chicken_price <= MAX_Chicken_price {
+			chickenchannel <- websites
+			break
+		}
+	}
+
+}
+
+func sendMessage(chickenchannel chan string, tofuchannel chan string) {
+	fmt.Printf("\nFound a deal on chicken at %s", <-chickenchannel)
+	fmt.Printf("\nFound a deal on chicken at %s", <-tofuchannel)
 
 }
